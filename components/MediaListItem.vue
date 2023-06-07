a306792b1b86781e20d7097e764e389352f28a89
ying_123 <zhangying@aten-optics.com>
2023-06-07 14:16:38 +0800

    fix text overflow error

--- components/MediaListItem.vue
+++ components/MediaListItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-stretch p-2 gap-x-2 bg-white dark:bg-black shadow shadow-black/25 rounded">
         <UAvatar class="shrink-0" :src="src" size="xl" />
-        <div class="flex flex-col justify-around grow">
+        <div class="flex flex-col justify-around overflow-hidden grow">
             <p class="whitespace-nowrap overflow-hidden text-ellipsis">{{ title }}</p>
             <p class="opacity-70 text-sm">{{ subtitle }}</p>
         </div>
