1a74ca43ea40548fa8ec266238d516a57aeb5f21
ying_123 <zhangying@aten-optics.com>
2023-06-25 09:03:07 +0800

    update viewport

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
     <GlobalProvider>
+        <Meta name="viewport"
+            content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
         <slot />
     </GlobalProvider>
 </template>
