066ec8a7a300ceeb2e737194ce02688f8414b766
ying_123 <zhangying@aten-optics.com>
2023-09-21 15:00:14 +0800

    update overlay style

--- components/LoadingOverlay.vue
+++ components/LoadingOverlay.vue
@@ -2,7 +2,7 @@
     <div class="absolute flex justify-center items-center inset-0 z-[100]" :class="{
         'bg-black/25': backdrop
     }">
-        <div class="flex items-center rounded p-3 space-x-2 bg-black/75 text-white">
+        <div class="flex items-center rounded p-3 space-x-4 bg-black/75 text-white">
             <Spinner class="text-4xl text-primary-500 dark:text-primary-400" />
             <span>加载中..</span>
         </div>
